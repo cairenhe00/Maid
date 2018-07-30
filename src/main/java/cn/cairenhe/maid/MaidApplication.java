2a855706c6210cc82c3df2f62afa12d135e67a06
cairenhe <ciarenhe00@163.com>
2018-07-31 01:48:14 +0800

    !

--- src/main/java/cn/cairenhe/maid/MaidApplication.java
+++ src/main/java/cn/cairenhe/maid/MaidApplication.java
@@ -8,6 +8,6 @@ public class MaidApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MaidApplication.class, args);
-        System.out.println("hello world");
+        System.out.println("hello world!");
     }
 }
