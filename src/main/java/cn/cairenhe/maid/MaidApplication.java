9d36b83122ee748ef66280ae7bcaf585b5b09b28
cairenhe00 <cairenhe00@163.com>
2018-07-31 17:02:27 +0800

    hello方法可用

--- src/main/java/cn/cairenhe/maid/MaidApplication.java
+++ src/main/java/cn/cairenhe/maid/MaidApplication.java
@@ -3,11 +3,10 @@ package cn.cairenhe.maid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"cn.cairenhe.controller"})
 public class MaidApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(MaidApplication.class, args);
-        System.out.println("hello world!");
     }
 }
